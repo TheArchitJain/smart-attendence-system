e77c1ecfc0bf41276504d3e5972dd1d3de054e36
Shashwat Jain <kcapshashwat@gmail.com>
2024-04-30 16:11:55 +0530

    changes in image taking process for better accuracy:

--- takeImage.py
+++ takeImage.py
@@ -30,8 +30,8 @@ def TakeImage(l1, l2, haarcasecade_path, trainimage_path, message, err_screen,te
             os.mkdir(path)
             while True:
                 ret, img = cam.read()
-                gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                faces = detector.detectMultiScale(gray, 1.3, 5)
+                # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                faces = detector.detectMultiScale(img, 1.3, 5)
                 for (x, y, w, h) in faces:
                     cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
                     sampleNum = sampleNum + 1
@@ -43,7 +43,7 @@ def TakeImage(l1, l2, haarcasecade_path, trainimage_path, message, err_screen,te
                         + "_"
                         + str(sampleNum)
                         + ".jpg",
-                        gray[y : y + h, x : x + w],
+                        img[y : y + h, x : x + w],
                     )
                     cv2.imshow("Frame", img)
                 if cv2.waitKey(1) & 0xFF == ord("q"):
